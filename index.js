2f3211ceb4d7210cbc01a1ccdfd46065987a9dd9
Jack Yeung <jyeung.se@gmail.com>
2018-05-28 23:30:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"]
 
+var destructiveAppendKitten = function(name) {
+  var kittens = kittens.push(name);
+  return kittens;
+}
